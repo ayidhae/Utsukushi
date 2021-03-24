4dee076195791dc7e0e45d8841d02eca7a308adb
ayidhae <ayidhae@gmail.com>
2021-03-24 12:35:58 +0700

    test

---
+++ application/views/welcome_message.php
@@ -0,0 +1,118 @@
+<!DOCTYPE HTML>
+<!--
+	Spatial by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+
+<html>
+	<head>
+		<title>Home | Klinik Utsukushi</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="css/main.css" />
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+
+				<div class="image" style="padding-top: 20px; padding-left: 20px"><img src=""  /></div>
+
+				<nav id="nav">
+					<ul>
+						 
+						<li><a href="C_login">Login</a></li>
+
+
+
+					</ul>
+				</nav>
+			</header>
+
+			<a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+
+		<!-- Banner -->
+			<section id="banner">
+				<h2>Selamat Datang</h2>
+				
+			</section>
+
+			<!-- One -->
+				<section id="one" class="wrapper style1">
+					<div class="container 75%">
+						<div class="row 200%">
+							<div class="6u 12u$(medium)">
+								<header class="major">
+																
+							
+							</div>
+							<div class="isi">
+							<h2>VISI MISI</h2>
+	
+</div>
+						</div>
+					</div>
+						</header>
+				</section>
+
+			<!-- Two -->
+				<section id="two" class="wrapper style2 special">
+					<div class="container">
+						<header class="major">
+						<center><h2></h2></center>				
+						</header>
+						<div class="feature-grid">
+							<div class="feature">
+								<div class="w3-display-topleft w3-container" class="col-xs-12  col-sm-4  col-lg-2"><img src=" width="300" alt="" style="width:50%"/></div>								
+							</div>
+							<div class="feature">
+								<div class="w3-display-topright w3-container" class="col-xs-12  col-sm-4  col-lg-2"><img src="" width="300" alt=""  style="width:50%" /></div>								
+							</div>
+							<div class="feature">
+								<div  class="w3-display-bottomleft w3-container" class="col-xs-12  col-sm-4  col-lg-2"><img src="" alt=""  style="width:50%" /></div>								
+							</div>
+							<div class="feature">
+								<div  class="w3-display-bottomright w3-container" class="col-xs-12  col-sm-4  col-lg-2"><img src="" alt=""  style="width:50%" /></div>								
+							</div>
+						</div>
+					</div>
+				</section>
+
+		
+			<!-- Four -->
+				<section id="four" class="wrapper style3 special">
+					<div class="container">
+						<header class="major">
+							
+						</header>
+						<ul class="actions">
+							<!-- <li><a href="#" class="button special big">Get in touch</a></li> -->
+						</ul>
+					</div>
+				</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<div class="col-xs-12  col-md-4"><div class="widget  widget_text"><h6 class="footer-top__headings">CONTACT US</h6>			<div class="textwidget"><p><span class="
+				<p><a class="icon-container" href="#" target="_self">
+				</div>
+		</div>
+		</div>				
+		</div>
+			</div>
+		</div>
+				</div>
+			</footer>
+
+			
+
+		<!-- Scripts -->
+			<script src="asset/js/jquery.min.js"></script>
+			<script src="asset/js/skel.min.js"></script>
+			<script src="asset/js/util.js"></script>
+			<script src="asset/js/main.js"></script>
+
+	</body>
+</html>
